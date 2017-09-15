6998329a3f2066037b0e4d8f57df3c7ab2e246b5
Kruk939 <kajetan@smsnet.pl>
2017-09-15 23:55:11 +0200

    Handcuff item

--- addons/captives/CfgWeapons.hpp
+++ addons/captives/CfgWeapons.hpp
@@ -12,4 +12,24 @@ class CfgWeapons {
             mass = 1;
         };
     };
+    class ACE_Handcuffs: ACE_ItemCore {
+        displayName = CSTRING(Handcuffs);
+        descriptionShort = CSTRING(HandcuffsDescription);
+        //model = QPATHTOF(models\ace_cabletie.p3d);
+        picture = QPATHTOF(UI\ace_handcuff_ca.paa);
+        scope = 2;
+        class ItemInfo: CBA_MiscItem_ItemInfo {
+            mass = 1;
+        };
+   };
+   class ACE_HandcuffsKey: ACE_ItemCore {
+      displayName = CSTRING(HandcuffsKeys);
+      descriptionShort = CSTRING(HandcuffsKeysDescription);
+      //model = QPATHTOF(models\ace_cabletie.p3d);
+      picture = QPATHTOF(UI\ace_handcuff_keys_ca.paa);
+      scope = 2;
+      class ItemInfo: CBA_MiscItem_ItemInfo {
+           mass = 1;
+      };
+  };
 };
